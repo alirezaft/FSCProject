a34287aa9b1f50098132bd6b7cc7e513d60ef28b
MostafaGhr <mostafa_gho@yahoo.com>
2020-01-29 22:01:34 +0330

    command fix

--- Q1.js
+++ Q1.js
@@ -2,8 +2,11 @@ const { exec } = require('child_process');
 
 const addIP4SSH = (ip) => {
     console.log(ip);
-    exec('sudo iptables -A INPUT -p tcp -m tcp --dport 22 -s ' + ip + ' -j ACCEPT');
-    exec('sudo iptables -A INPUT -p tcp -m tcp --dport 22 -s -j DROP');
+    // exec('sudo iptables -A INPUT -p tcp -m tcp --dport 22 -s ' + ip + ' -j ACCEPT');
+    // exec('sudo iptables -A INPUT -p tcp -m tcp --dport 22 -s -j DROP');
+    exec('iptables -D INPUT -p tcp --dport 22 -j DROP');
+    exec('iptables -A INPUT -s ' + ip + ' -j ACCEPT');
+    exec('iptables -A INPUT -p tcp --dport 22 -j DROP');
 }
 
 exports.addssh = addIP4SSH;
