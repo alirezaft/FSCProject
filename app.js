889dfc835f02e7c050ed5375018d87dfa45907d7
MostafaGhr <mostafa_gho@yahoo.com>
2020-01-30 01:19:38 +0330

    6463 discord

--- app.js
+++ app.js
@@ -42,8 +42,8 @@ app.get('/checkme', (req,res) => {
             res.send(str);
         }
     });
-    portcheck.CheckMyPort(5353, req.ip, (ans) => {
-        str = str.concat('port 5353: ' + ans + '<br>');
+    portcheck.CheckMyPort(6463, req.ip, (ans) => {
+        str = str.concat('port 6463: ' + ans + '<br>');
         console.log(str)
         if((str.match(/<br>/g) || []).length == 4){
             res.send(str);
